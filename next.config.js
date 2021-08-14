ba991024b66fe9b5100e3fd9f9797d153fb6c77b
KealingIt <88913739+KealingIt@users.noreply.github.com>
2021-08-13 21:12:24 -0500

    💚  Add next-optimized-images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-module.exports = {
+const withOptimizedImages = require('next-optimized-images')
+
+module.exports = withOptimizedImages({
   reactStrictMode: true,
-  images: {
-    domains: ['drive.google.com'],
-  }
-}
+  handleImages: ['jpeg', 'png', 'svg'],
+})
