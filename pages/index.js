e2244d8b03944e6f31017a8908c05914fe6b1d70
KealingIt <88913739+KealingIt@users.noreply.github.com>
2021-08-14 12:47:25 -0500

    🐛  Add base route to index redirect

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 
 export default function Home({ content }) {
   useEffect(() => {
-    location.href = "/feed"
+    location.href = "/web/feed"
   }, [])
 
   return (<Head><title>Feed - KNN</title></Head>)
